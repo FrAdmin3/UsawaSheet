b6f3796efde7c699e23dd81eb3dbc3edc683048b
FrAdmin3 <17km141@esisalama.org>
2025-02-16 11:54:55 +0200

    Add files via upload

---
+++ pyodbc_recipe.py
@@ -0,0 +1,12 @@
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
+
+class PyodbcRecipe(CompiledComponentsPythonRecipe):
+    version = '4.0.30'
+    url = 'https://github.com/mkleehammer/pyodbc/archive/{version}.tar.gz'
+    site_packages_name = 'pyodbc'
+    depends = ['setuptools']
+    call_hostpython_via_targetpython = False
+    install_in_hostpython = True
+    install_in_targetpython = False
+
+recipe = PyodbcRecipe()
